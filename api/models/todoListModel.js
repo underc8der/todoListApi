8527aa1396d1e578f033f60d4614f1e119e793c8
maria eugenia acevedo ochoa <maria eugenia acevedo ochoa>
2017-04-18 21:20:52 -0500

    mongoose configuration new task model schema

---
+++ api/models/todoListModel.js
@@ -0,0 +1,23 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var TaskSchema = new Schema({
+   name: {
+      type: String,
+      require: 'kindly enter the name of the task'
+   },
+   create_date: {
+      type: Date,
+      default: Date.now
+   },
+   status: {
+      type: [{
+         type: String,
+	 enum: ['pending','ongoing','comlpeted']
+      }],
+      default: ['pending']	   
+   }
+});
+
+module.exports = mongoose.model('Task', TaskSchema);
